1a363af8f57f0acf96be89542c9f840dfca97244
rodrigojmayer <rodrigojmayer@gmail.com>
2025-10-31 11:32:33 -0300

    ManageAlert date picker component

--- src/components/ManageAlert.tsx
+++ src/components/ManageAlert.tsx
@@ -1,13 +1,30 @@
 "use client";
+import dayjs, { Dayjs } from 'dayjs';// Import dayjs
 import { useState } from "react";
+import { Box,
+         Grid,
+         TextField,
+         Typography,
+         InputAdornment,
+        } from '@mui/material';
 import { patchAlert, postAlert } from "../lib/api";
 import type { Alert, NewAlert } from "../types";
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { MobileDatePicker } from "@mui/x-date-pickers/MobileDatePicker";
+import useMediaQuery from '@mui/material/useMediaQuery'
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import CalendarMonthRoundedIcon from '@mui/icons-material/CalendarMonthRounded';
 
 interface ManageAlertProps {
     alertEdit?: Alert;
     setVisibleUpdateAlert?: (visible: boolean) => void;
 }
 export default function ManageAlert({ alertEdit, setVisibleUpdateAlert }: ManageAlertProps) {
+    
+    const breakpointLG = useMediaQuery('(min-width:1024px)')
+    const DatePickerComponent = breakpointLG ? DatePicker : MobileDatePicker;
     const [alertIdDebt, setAlertIdDebt] = useState<string>(alertEdit ? alertEdit.id_debt : "");
     const [alertDateAlert, setAlertDateAlert] = useState<Date | null>(alertEdit ? new Date(alertEdit.date_alert) : null);
     const [alertSent, setAlertSent] = useState<boolean>(alertEdit ? alertEdit.sent : false);
@@ -16,6 +33,7 @@ export default function ManageAlert({ alertEdit, setVisibleUpdateAlert }: Manage
     
     const [loading, setLoading] = useState(false);
     const [message, setMessage] = useState<string | null>(null);
+    const [openDatePicker, setOpenDatePicker] = useState(false);  
 
     async function handleSubmit (e: React.FormEvent) {
         e.preventDefault();
@@ -55,32 +73,78 @@ export default function ManageAlert({ alertEdit, setVisibleUpdateAlert }: Manage
         }
     }
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const handleDatePickerChange = (newDate:any) => { 
+        const adjustedDate = newDate.add(2, 'hour').toISOString(); // Adding 2 hours because the GMT comes in +0200 and returns the day before
+        setAlertDateAlert(adjustedDate);
+      };
     return(
         <div className="flex flex-col p-2 max-w-md">
             <h2 className="text-3xl font-bold text-gray-800 dark:text-gray-100 mb-4">
                 {alertEdit ? "Update" : "Create"} Alert
             </h2>
 
-            <form onSubmit={handleSubmit} className="flex flex-col gap-4 border rounded-lg p-2 bg-red-300">
+            <form onSubmit={handleSubmit} className="flex flex-col gap-1.5 border rounded-lg p-2 bg-red-300 text-gray-800">
+                <label className="input-label">ID Debt</label>
                 <input
                     type="text"
-                    placeholder="ID Debt"
+                    placeholder=" "
                     value={alertIdDebt}
                     onChange={(e) => setAlertIdDebt(e.target.value)}
-                    className="border rounded-lg p-2 bg-white text-gray-800"
+                    className="border rounded-lg p-2 bg-white"
                     required
                 />
-                <input
-                    type="date"
-                    placeholder="Date Alert"
-                    value={alertDateAlert && alertDateAlert.toString() !== "Invalid Date" ? alertDateAlert.toISOString().slice(0, 10) : ""}
-                    onChange={(e) => setAlertDateAlert(e.target.value ? new Date(e.target.value) : null)}
-                    className="border rounded-lg p-2 bg-white text-gray-800"
-                />
+                <label className="input-label">Date Alert</label>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                            <DemoContainer components={['DatePicker']} >
+                                <DatePickerComponent
+                                    className="border rounded-lg p-2 bg-white"
+                                    format="DD/MM/YYYY"
+                                    defaultValue = { alertDateAlert? dayjs(alertDateAlert) : null}
+                                    onChange={ (newDate) => handleDatePickerChange(newDate) }
+                                    slotProps={{
+                                        textField: {
+                                            size: 'small',
+                                            InputProps: {
+                                            endAdornment: (
+                                            <InputAdornment
+                                                position="end"
+                                            >
+                                            <CalendarMonthRoundedIcon 
+                                                onClick = {() => setOpenDatePicker(true)}
+                                                style={{cursor: "pointer"} }
+                                            />
+                                            </InputAdornment>
+                                            ),
+                                            },
+                                        },
+                                    }}
+                                    sx={{ 
+                                        marginTop: "-8px !important"
+                                    }} 
+                                    open={breakpointLG ? openDatePicker :undefined}
+                                    onClose={breakpointLG ? () => setOpenDatePicker(false) :()=>{}}
+                                />
+                            </DemoContainer>
+                        </LocalizationProvider>
+                    {/* <input
+                        type="date"
+                        id="date_alert"
+                        placeholder=" "
+                        value={
+                            alertDateAlert && alertDateAlert.toString() !== "Invalid Date"
+                             ? alertDateAlert.toISOString().slice(0, 10)
+                              :  ""
+                        }
+                        onChange={(e) => 
+                            setAlertDateAlert(e.target.value ? new Date(e.target.value) : null)
+                        }
+                        className="border rounded-lg p-2 bg-white"
+                    /> */}
                 { alertEdit ?
                     <>
                         <label className="grid grid-cols-[1fr_auto] items-center gap-x-4 w-24">
-                            <span className="text-sm text-gray-800 truncate">Sent</span>
+                            <span className="text-sm truncate">Sent</span>
                             <input
                                 type="checkbox"
                                 checked={alertSent}
@@ -95,7 +159,7 @@ export default function ManageAlert({ alertEdit, setVisibleUpdateAlert }: Manage
                             />
                         </label>
                         <label className="grid grid-cols-[1fr_auto] items-center gap-x-4 w-24">
-                            <span className="text-sm text-gray-800 truncate">Enabled</span>
+                            <span className="text-sm truncate">Enabled</span>
                             <input
                                 type="checkbox"
                                 checked={alertEnabled}
@@ -110,7 +174,7 @@ export default function ManageAlert({ alertEdit, setVisibleUpdateAlert }: Manage
                             />
                         </label>
                         <label className="grid grid-cols-[1fr_auto] items-center gap-x-4 w-24">
-                            <span className="text-sm text-gray-800 truncate">Deleted</span>
+                            <span className="text-sm truncate">Deleted</span>
                             <input
                                 type="checkbox"
                                 checked={alertDeleted}
