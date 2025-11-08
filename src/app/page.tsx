0ef339ff3b8704bda0e9b305b7f1a8c36a34c33a
rodrigojmayer <rodrigojmayer@gmail.com>
2025-11-08 01:30:03 -0300

    base colors global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@
 export default function Home() {
   return (
     <div className="flex items-center justify-center bg-slate-100">
-      <h1 className="text-4xl font-bold text-blue-600">Cuentas claras</h1>
+      
       {/* < DebtsList /> */}
     </div>
     );
