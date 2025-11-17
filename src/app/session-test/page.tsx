e8e306aa307bd797518c9d56ac4e755c3ea6e1a0
rodrigojmayer <rodrigojmayer@gmail.com>
2025-11-16 21:54:11 -0300

    session test implementation

--- src/app/session-test/page.tsx
+++ src/app/session-test/page.tsx
@@ -1,7 +1,28 @@
-"use client"
+"use client";
+
+import { useStylesGlobal } from "@/Styles";
+import { signIn } from "next-auth/react";
+import { signOut } from "next-auth/react";
 import { useSession } from "next-auth/react"
 
-export default function TestSession() {
+export default function SessionTestPage() {
+  const { classes } = useStylesGlobal()
     const { data: session } = useSession()
-    return <pre>{JSON.stringify(session, null, 2)}</pre>
+    return (
+        <div className={classes.table_header_color}>
+            <div>
+                <button onClick={() => signIn("google")} className="bg-blue-500 py-2 m-3 w-30 rounded-lg hover:bg-gray-700 transition cursor-pointer">
+                    Iniciar sesión
+                </button>
+            </div>
+            <div>
+                <button onClick={() => signOut()} className="bg-red-500 py-2 m-3 w-30 rounded-lg hover:bg-gray-700 transition cursor-pointer">
+                    Cerrar sesión
+                </button>
+            </div>
+            <div>
+                <pre className="py-2 m-3">{JSON.stringify(session, null, 2)}</pre>
+            </div>
+        </div>
+    );
 }
